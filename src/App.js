f94e3727f761195b953af03507559c36eba7f3f7
bikashkatwal <bikashkatwal9@gmail.com>
2017-11-17 10:08:41 +1100

    Enabling & Using CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import  Person from './Person/Person';
 
 class App extends Component {
@@ -71,18 +71,18 @@ class App extends Component {
           inlineStyle.backgroundColor='red';
       }
 
-      const classes=[];
+      const assignedClasses=[];
       if(this.state.persons.length<= 2){
-          classes.push('red');// classes=['red']
+          assignedClasses.push(classes.red);// assignedClasses=['red']
       }
       if(this.state.persons.length<= 1){
-          classes.push('bold');// classes=['red','bold']
+          assignedClasses.push(classes.bold);// assignedClasses=['red','bold']
       }
 
     return (
-          <div className="App">
+          <div className={classes.App}>
             <h2>Hi, I'm React App</h2>
-              <p className={classes.join(' ')}>This is really working</p>
+              <p className={assignedClasses.join(' ')}>This is really working</p>
               <button
                   style={inlineStyle}
                   onClick={this.togglePersonHandler}>Toggle Person</button>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) =>{
     const style={
@@ -10,7 +10,7 @@ const person = (props) =>{
     };
 
   return (
-      <div className="Person" >
+      <div className={classes.Person} >
         <p onClick={props.click}>
             I'm a {props.name} and I am {props.age} year old and my </p>
           <p>{props.children}</p>
