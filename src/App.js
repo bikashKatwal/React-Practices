545dd72d3e16cd8dab1be08af7bacf5558b1ccd5
bikashkatwal <bikashkatwal9@gmail.com>
2017-11-17 00:41:52 +1100

    'Adding-using-Radium'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import  Person from './Person/Person';
 
 class App extends Component {
@@ -52,7 +53,11 @@ class App extends Component {
           font:'inherit',
           border: '1px solid blue',
           padding: '8px',
-          cursor: 'pointer'
+          cursor: 'pointer',
+          ':hover':{
+            backgroundColor: 'lightgreen',
+              color:'black'
+          }
       };
 
       let persons=null;
@@ -70,6 +75,10 @@ class App extends Component {
               </div>
           );
           inlineStyle.backgroundColor='red';
+          inlineStyle[':hover']={
+              backgroundColor: 'salmon',
+                  color:'black'
+          }
       }
 
       const classes=[];
@@ -92,4 +101,4 @@ class App extends Component {
     );
   }
 }
-export default App;
+export default Radium(App);
