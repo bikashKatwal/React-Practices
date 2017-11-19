f4c3703ae87994ce05501c01acccbfaff8782bf8
bikashkatwal <bikashkatwal9@gmail.com>
2017-11-20 10:22:40 +1100

    added Higher Order Components(HOC)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Hoc from '../../hoc/Hoc';
 
 const cockpit =(props)=>{
     const assignedClasses=[];
-    let btnClass='';
+    let btnClass=classes.Button;
     if(props.showPersons){
-        btnClass=classes.Red;
+        btnClass=[classes.Button,classes.Red].join(' ');
     }
 
     if(props.persons.length<= 2){
@@ -15,13 +16,15 @@ const cockpit =(props)=>{
         assignedClasses.push(classes.bold);// assignedClasses=['red','bold']
     }
 
-  return(
-      <div className={classes.Cockpit}>
-          <h2>{props.appTitle}</h2>
-          <p className={assignedClasses.join(' ')}>This is really working</p>
-          <button className={btnClass}
-                  onClick={props.clicked}>Toggle Person</button>
-      </div>
-  );
+    return(
+        <Hoc>
+            <h2>{props.appTitle}</h2>
+            <p className={assignedClasses.join(' ')}>This is really working</p>
+            <button className={btnClass}
+                    onClick={props.clicked}>Toggle Person</button>
+        </Hoc>
+    );
+
+
 };
 export default cockpit;
